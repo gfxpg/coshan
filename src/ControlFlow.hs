bf36434dd87f790c8e6bf8c2bcfd07c7f4c94856
timlathy <tim.labushev@gmail.com>
2020-03-09 09:39:18 +0300

    recursively build up basic blocks

--- src/ControlFlow.hs
+++ src/ControlFlow.hs
@@ -4,32 +4,49 @@ module ControlFlow
 where
 
 import           Data.List
-import           Parser.Types                   ( Instruction(..) )
+import           Parser.Types                   ( Instruction(..)
+                                                , Operand(..)
+                                                )
 
 -- label, instructions
 data BasicBlock = BasicBlock String [Instruction]
   deriving (Show)
 
-data BlockEdge = BlockEdgeT String
-               | BlockEdgeF String
+data BlockEdge = BlockEdgeT String String
+               | BlockEdgeF String String
   deriving (Show)
 
 type CFG = ([BasicBlock], [BlockEdge])
 
 constructGraph :: [Instruction] -> CFG
-constructGraph is = ([firstBlock], [])
-  where (firstBlock, _) = scanBasicBlock is 0
+constructGraph = scanBlocks ([], []) 0
 
-scanBasicBlock :: [Instruction] -> Int -> (BasicBlock, [Instruction])
-scanBasicBlock insts id = (BasicBlock name block, rest)
+scanBlocks :: CFG -> Int -> [Instruction] -> CFG
+scanBlocks (blocks, edges) i insts = case scanBasicBlock i insts of
+  (block, []  ) -> (block : blocks, edges)
+  (block, rest) -> (block : newBlocks, e1 : e0 : edges)
+   where
+    (newBlocks, newEdges)     = scanBlocks (blocks, edges) (i + 1) rest
+    e1                        = BlockEdgeT bbName $ branchLabel $ last bbInsts
+    e0                        = BlockEdgeF bbName $ blockName $ i + 1
+    BasicBlock bbName bbInsts = block
+    branchLabel (AsmInstr _ [OpSys label]) = label
+
+scanBasicBlock :: Int -> [Instruction] -> (BasicBlock, [Instruction])
+scanBasicBlock id insts = (BasicBlock name block, rest)
  where
-  (block, rest) = break branchInst body
-  (name , body) = case insts of
+  (block, rest) = case break isBranch body of
+    (block, br@(AsmInstr _ _) : rest) -> (block ++ [br], rest)
+    (block, rest                    ) -> (block, rest)
+  (name, body) = case insts of
     (AsmLabel label) : rest -> (label, rest)
-    _                       -> (show id, insts)
+    _                       -> (blockName id, insts)
 
-branchInst :: Instruction -> Bool
+blockName :: Int -> String
+blockName i = "BB" ++ show i
+
+isBranch :: Instruction -> Bool
 -- EXEC-modifying instructions are not recognized as branches for now
-branchInst (AsmInstr ('v' : _) _) = False
-branchInst (AsmInstr inst      _) = "s_cbranch" `isPrefixOf` inst
-branchInst _                      = False
+isBranch (AsmInstr ('v' : _) _) = False
+isBranch (AsmInstr inst      _) = "s_cbranch" `isPrefixOf` inst
+isBranch (AsmLabel _          ) = True
