d625ebaf97ce5429d60416ef8c0aee0b526b064e
timlathy <tim.labushev@gmail.com>
2020-03-09 10:43:28 +0300

    simplify parser tests

--- test/Helper.hs
+++ test/Helper.hs
@@ -23,13 +23,11 @@ runControlFlowCase casefile = do
   outputPath = "test/ControlFlow/Cases/" ++ casefile
   inputPath  = outputPath ++ ".isa"
 
-runParserCase
-  :: (Read s, Show s, Eq s)
-  => ShowErrorComponent e => Parsec e String s -> String -> Expectation
-runParserCase parser casefile = do
+runParserCase :: String -> Expectation
+runParserCase casefile = do
   input  <- readFile inputPath
   output <- read <$> readFile outputPath
-  parse parser casefile input `shouldParse` output
+  parse asmlst casefile input `shouldParse` output
  where
   outputPath = "test/Parser/Cases/" ++ casefile
   inputPath  = outputPath ++ ".isa"
--- test/Parser/GeneralSpec.hs
+++ test/Parser/GeneralSpec.hs
@@ -1,16 +1,9 @@
 module Parser.GeneralSpec where
 
 import           Test.Hspec
-import           Test.Hspec.Megaparsec
-
-import           Text.Megaparsec                ( parse )
-
 import           Helper                         ( runParserCase )
-import           Parser                         ( asmlst
-                                                , Listing(..)
-                                                )
+
 
 spec :: Spec
 spec = describe "parser" $ do
-  it "handles a simple case" $ do
-    runParserCase asmlst "simple"
+  it "handles a simple case" $ runParserCase "simple"
