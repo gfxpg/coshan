ba7c657fcdd12c2cf93056c1afd1373d320865db
timlathy <tim.labushev@gmail.com>
2021-01-17 23:08:29 +0300

    read elfs produced by clang -x assembler

--- src/Disassembler.hs
+++ src/Disassembler.hs
@@ -12,18 +12,18 @@ import Disassembler.Types
 readElf :: DisasmTarget -> ByteString -> IO [DisassembledKernel]
 readElf target bin = do
   llvm <- getLlvmRef target
-  sequence (disasmSymbol llvm <$> kernelSymbols)
+  sequence $ disasmSymbol llvm <$> kernelSymbols
   where
     disasmSymbol llvm sym = do
       let section = fromJust $ E.steEnclosingSection sym
-      let dataSize = fromIntegral $ E.steSize sym
+      let dataSize = fromIntegral $ if E.steSize sym > 0 then E.steSize sym else E.elfSectionSize section
       let dataOffset = fromIntegral $ E.steValue sym - E.elfSectionAddr section
       let kernelData = BStr.take dataSize $ BStr.drop dataOffset $ E.elfSectionData section
       let (disasmKernelCodeT, disasmInstructionsBin) = BStr.splitAt 256 kernelData
       let disasmKernelName = fromJust $ snd $ E.steName sym
       disasmInstructions <- disassemble llvm disasmInstructionsBin
       pure $ DisassembledKernel {disasmKernelName, disasmKernelCodeT, disasmInstructionsBin, disasmInstructions}
-    kernelSymbols = filter isCodeSymbol $ head $ E.parseSymbolTables $ elf
+    kernelSymbols = filter isCodeSymbol . head . E.parseSymbolTables $ elf
     isCodeSymbol sym = case E.steEnclosingSection sym of
       Just sct -> E.elfSectionName sct == ".text"
       Nothing -> False
