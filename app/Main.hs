314110d4b87fb832b56f4cc720e71d69fcbab232
timlathy <tim.labushev@gmail.com>
2021-02-08 21:03:47 +0300

    cli: add an option to insert nops at specified locations to simplify manual testing on known kernels (e.g. by replacing s_waitcnts with nops)

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 {-# OPTIONS_GHC -fno-cse #-}
 
 module Main where
@@ -11,12 +13,14 @@ import Coshan.Reporting
 import Data.Bifunctor (second)
 import qualified Data.ByteString as B
 import Format.Cfg (printCfg)
+import Numeric (readHex)
 import System.Console.CmdArgs
 
 data Opts = Opts
   { optCpu, optTriple :: String,
     optCo :: FilePath,
-    optDisasm :: Bool
+    optDisasm :: Bool,
+    optNops :: [String]
   }
   deriving (Data, Typeable, Show)
 
@@ -25,8 +29,9 @@ opts =
   Opts
     { optCpu = "gfx900" &= explicit &= name "mcpu" &= typ "gfx900" &= help "Target CPU type for disassembly (run `llc -march=amdgcn -mcpu=help` for a list of available CPUs)",
       optTriple = "amdgcn--amdhsa" &= explicit &= name "triple" &= typ "amdgcn--amdhsa" &= help "Target triple for disassembly",
+      optCo = def &= argPos 0 &= typFile,
       optDisasm = def &= explicit &= name "disasm" &= help "Print disassembled shader code and exit",
-      optCo = def &= argPos 0 &= typFile
+      optNops = def &= explicit &= name "nop" &= help "Replace all DWORDs of instructions at specified locations (hex) with `s_nop 0`. LIMITATIONS: does not affect the binary form of instructions."
     }
     &= program "coshan"
     &= summary "compute shader analyzer 0.1"
@@ -37,9 +42,10 @@ main = do
   args <- cmdArgs opts
 
   let target = DisasmTarget {disasmTriple = optTriple args, disasmCPU = optCpu args}
+  let nopLocations = fst . head . readHex <$> optNops args
 
   co <- B.readFile (optCo args)
-  kernel <- head <$> readElf target co
+  kernel <- insertNops nopLocations . head <$> readElf target co
 
   let instructions = second parseInstruction <$> disasmInstructions kernel
   let cfg = buildCfg instructions
@@ -50,6 +56,17 @@ main = do
     _ -> do
       report "waitcnt" (checkWaitcnts kernel cfg)
       report "s_nop" (checkRwLaneHazards kernel cfg)
+
+insertNops :: [Int] -> DisassembledKernel -> DisassembledKernel
+insertNops [] kernel = kernel
+insertNops nops kernel = kernel {disasmInstructions = scanInstructions (disasmInstructions kernel) []}
+  where
+    scanInstructions [] acc = reverse acc
+    scanInstructions ((pc, i) : rest) acc
+      | pc `elem` nops = scanInstructions rest $ case rest of
+        (nextPc, _) : _ -> ((,"s_nop 0") <$> [nextPc - 4, nextPc - 8 .. pc]) ++ acc
+        _ -> (pc, "s_nop 0") : acc
+      | otherwise = scanInstructions rest $ (pc, i) : acc
 
 report :: String -> [LogMessage] -> IO ()
 report analyzer [] = putStrLn (analyzer ++ ": OK")
