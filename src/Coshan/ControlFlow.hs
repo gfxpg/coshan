ce68782b76458db3e4bb3be675052aa7fb9653fe
timlathy <tim.labushev@gmail.com>
2021-02-13 12:20:58 +0300

    cfg: resolve s_call/s_setpc instruction pairs as branches

--- src/Coshan/ControlFlow.hs
+++ src/Coshan/ControlFlow.hs
@@ -1,9 +1,12 @@
 module Coshan.ControlFlow (buildCfg, CFG (..), BasicBlock (..), BasicBlockIdx) where
 
 import Coshan.Disassembler (Instruction (..), Operand (..), PC)
-import Data.List (findIndex, findIndices)
-import Data.Maybe (catMaybes)
+import Data.List (find, findIndex, findIndices, foldl')
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.Set (Set)
 import qualified Data.Set as Set
+import Debug.Trace
 
 type BasicBlockIdx = Int
 
@@ -20,46 +23,79 @@ buildCfg :: [(PC, Instruction)] -> CFG
 buildCfg instrs = CFG blocks''
   where
     blocks'' = (\(bbIdx, bb) -> bb {bbPredecessors = findIndices (elem bbIdx . bbSuccessors) blocks'}) <$> zip [0 ..] blocks'
-    blocks' = fillSuccs <$> blocks
+    blocks' = {- trace ("branches: " ++ show branches) $-} fillSuccs <$> blocks
       where
         fillSuccs (BasicBlock instructions _ _) = BasicBlock instructions [] succBbs
           where
-            (bbEndPc, bbLastInstr) = last instructions
-            succBbs = catMaybes $ immediateSucc : (successor <$> brs)
-            immediateSucc
-              | not bbEndsWithBranch = findIndex (\(BasicBlock ((startPc, _) : _) _ _) -> startPc > bbEndPc) blocks
-              | otherwise = Nothing
-            successor (brPc, brTargetPc, _)
-              | brPc == bbEndPc = findIndex (\(BasicBlock ((startPc, _) : _) _ _) -> startPc == brTargetPc) blocks
-              | otherwise = Nothing
-            bbEndsWithBranch = case bbLastInstr of
-              Instruction ["s", "branch"] _ -> True
-              Instruction ("s" : "cbranch" : _) _ -> True
-              _ -> False
-    blocks = reverse $ go [] blockStarts instrs
+            (bbEndPc, _) = last instructions
+            succBbs = case Map.lookup bbEndPc branches of
+              Just branchTargets ->
+                findIndices (\(BasicBlock ((startPc, _) : _) _ _) -> startPc `Set.member` branchTargets) blocks
+              _ -> case findIndex (\(BasicBlock ((startPc, _) : _) _ _) -> startPc > bbEndPc) blocks of
+                Just immediateSucc -> [immediateSucc]
+                _ -> []
+    blocks = {- trace ("starts: " ++ show blockStarts) $-} reverse $ go [] blockStarts instrs
       where
-        blockStarts = Set.toList $ Set.fromList $ 0 : ((\(_, target, _) -> target) <$> brs)
+        go bbs _ [] = bbs
         go bbs [_] is = BasicBlock is [] [] : bbs
         go bbs (_ : nextStartPc : rest) is =
           let (currInsts, nextBbInsts) = break ((== nextStartPc) . fst) is
            in go (BasicBlock currInsts [] [] : bbs) (nextStartPc : rest) nextBbInsts
-    brs = branches instrs
+        go _ _ _ = undefined
+    blockStarts = Set.toAscList (Map.foldlWithKey' (\brs sourcePc targets -> Set.insert (sourcePc + 4) $ Set.union targets brs) (Set.singleton 0) branches)
+    branches = scanBranches instrs
 
-branches :: [(PC, Instruction)] -> [(PC, PC, Bool)] -- (branch instruction pc, branch target pc, taken?)
-branches = go []
+scanBranches :: [(PC, Instruction)] -> Map PC (Set PC)
+scanBranches = resolveCallInstructions . scanBranchInstructions
+
+newtype SgprPair = SgprPair (Int, Int) deriving (Eq, Ord, Show)
+
+data Branches = Branches
+  { branchJumps :: Map PC (Set PC),
+    branchCalls :: [(PC, PC, SgprPair)],
+    branchReturns :: [(PC, SgprPair)]
+  }
+
+resolveCallInstructions :: Branches -> Map PC (Set PC)
+resolveCallInstructions branches = foldl' addCall (branchJumps branches) (branchCalls branches)
   where
-    go brs [] = brs
-    go brs ((pc, i) : instrs) = case i of
+    addCall acc (callPc, targetPc, callGprs) =
+      Map.insertWith Set.union callPc (Set.singleton targetPc) (followCall acc targetPc)
+      where
+        pcAfterExit = callPc + 4
+        followCall acc currentPc =
+          let closestRet = find (\(retPc, retGprs) -> retPc >= currentPc && retGprs == callGprs) (branchReturns branches)
+              closestBr = Map.lookupGE currentPc (branchJumps branches)
+           in case (closestRet, closestBr) of
+                (Nothing, _) ->
+                  acc -- TODO: this should probably be a warning (no s_setpc following s_call)
+                (Just (retPc, _), Just (brPc, brTargets))
+                  | retPc > brPc ->
+                    Set.foldl' followCall acc brTargets
+                (Just (retPc, _), _) ->
+                  Map.insertWith Set.union retPc (Set.singleton pcAfterExit) acc
+
+scanBranchInstructions :: [(PC, Instruction)] -> Branches
+scanBranchInstructions instructions = go instructions Branches {branchJumps = Map.empty, branchCalls = [], branchReturns = []}
+  where
+    go [] acc = acc
+    go ((pc, i) : rest) acc = go rest $ case i of
       Instruction opcode [OConst offset] ->
-        let branchOffset = if offset <= 32767 then offset else -1 * (65536 - offset)
-            nextPc = pc + 4
-            targetPc = nextPc + branchOffset * 4
-         in case opcode of
-              ["s", "branch"] ->
-                go ((pc, targetPc, True) : brs) instrs
-              "s" : "cbranch" : _ ->
-                go ((pc, targetPc, True) : (pc, nextPc, False) : brs) instrs
-              _ ->
-                go brs instrs
+        case opcode of
+          ["s", "branch"] ->
+            acc {branchJumps = Map.insert pc (Set.singleton (shortJumpTarget pc offset)) (branchJumps acc)}
+          "s" : "cbranch" : _ ->
+            acc {branchJumps = Map.insert pc (Set.fromList [shortJumpTarget pc offset, pc + 4]) (branchJumps acc)}
+          _ ->
+            acc
+      Instruction ["s", "call", "b64"] [Osgpr [s1, s2], OConst offset] ->
+        acc {branchCalls = (pc, shortJumpTarget pc offset, SgprPair (s1, s2)) : branchCalls acc}
+      Instruction ["s", "setpc", "b64"] [Osgpr [s1, s2]] ->
+        acc {branchReturns = (pc, SgprPair (s1, s2)) : branchReturns acc}
       _ ->
-        go brs instrs
+        acc
+
+shortJumpTarget :: PC -> Int -> PC
+shortJumpTarget pc offset
+  | offset <= 32767 = pc + 4 + 4 * offset
+  | otherwise = pc + 4 + 4 * ((-1) * (65536 - offset))
--- test/ControlFlow/GeneralSpec.hs
+++ test/ControlFlow/GeneralSpec.hs
@@ -8,41 +8,38 @@ import Data.String.Interpolate (i)
 import Helpers
 import Test.Hspec
 
-simpleLoopKernel :: IO (CFG, DisassembledKernel)
-simpleLoopKernel =
-  loadFirstKernel . gfx900Kernel "cfg_simple_loop" $
-    [i|
-s_load_dwordx2 s[0:1], s[4:5], 0x0
-s_waitcnt lgkmcnt(0)
-s_load_dword s2, s[0:1], 0x0
-s_waitcnt lgkmcnt(0)
-v_mov_b32_e32 v0, s2
-bb28:
-  v_add_f32_e32 v0, 1.0, v0
-  v_cmp_lt_f32_e32 vcc, 1.0, v0
-  s_and_b64 vcc, exec, vcc
-  s_cbranch_vccnz bb28
-bb44:
-  s_load_dword s2, s[0:1], 0x4
-  s_waitcnt lgkmcnt(0)
-  v_mov_b32_e32 v1, s2
-bb60:
-  v_add_f32_e32 v0, 1.0, v0
-  v_cmp_nlt_f32_e32 vcc, 1.0, v0
-  v_add_f32_e32 v1, 0x40a00000, v1
-  s_and_b64 vcc, exec, vcc
-  s_cbranch_vccnz bb60
-bb84:
-  v_mov_b32_e32 v3, s1
-  v_mov_b32_e32 v2, s0
-  global_store_dword v[2:3], v1, off offset:4
-  s_branch bb28
-|]
-
 spec :: Spec
 spec = describe "cfg construction" $ do
-  it "handles primitive loops" $ do
-    (cfg, _) <- simpleLoopKernel
+  it "recognizes scalar branching, including loops" $ do
+    (cfg, _) <-
+      loadFirstKernel . gfx900Kernel "cfg_simple_loop" $
+        [i|
+          s_load_dwordx2 s[0:1], s[4:5], 0x0
+          s_waitcnt lgkmcnt(0)
+          s_load_dword s2, s[0:1], 0x0
+          s_waitcnt lgkmcnt(0)
+          v_mov_b32_e32 v0, s2
+          bb28:
+            v_add_f32_e32 v0, 1.0, v0
+            v_cmp_lt_f32_e32 vcc, 1.0, v0
+            s_and_b64 vcc, exec, vcc
+            s_cbranch_vccnz bb28
+          bb44:
+            s_load_dword s2, s[0:1], 0x4
+            s_waitcnt lgkmcnt(0)
+            v_mov_b32_e32 v1, s2
+          bb60:
+            v_add_f32_e32 v0, 1.0, v0
+            v_cmp_nlt_f32_e32 vcc, 1.0, v0
+            v_add_f32_e32 v1, 0x40a00000, v1
+            s_and_b64 vcc, exec, vcc
+            s_cbranch_vccnz bb60
+          bb84:
+            v_mov_b32_e32 v3, s1
+            v_mov_b32_e32 v2, s0
+            global_store_dword v[2:3], v1, off offset:4
+            s_branch bb28
+        |]
     cfg
       `shouldBe` CFG
         [ BasicBlock
@@ -95,5 +92,121 @@ spec = describe "cfg construction" $ do
                 ],
               bbPredecessors = [3],
               bbSuccessors = [1]
+            }
+        ]
+  it "recognizes function calls that use s_call_b64" $ do
+    (cfg, _) <-
+      loadFirstKernel . gfx900Kernel "cfg_s_call" $
+        [i|
+          bb0:
+          s_load_dwordx2 s[0:1], s[4:5], 0x0
+          s_call_b64 s[10:11], fun1
+          v_cmp_lt_f32_e32 vcc, 1.0, v0
+          s_cbranch_vccnz bb0
+
+          bb1:
+          global_store_dword v[2:3], v0, off offset:4
+          s_call_b64 s[10:11], fun1
+          s_call_b64 s[0:1], fun2
+          s_cmp_eq_u32 s0, 0
+          s_cbranch_scc0 bb0
+          s_branch end
+
+          fun2:
+          s_setpc_b64 s[0:1]
+
+          fun1:
+          s_waitcnt lgkmcnt(0)
+          s_cmp_eq_u32 s1, 0
+          s_cbranch_scc1 fun1_end
+          s_call_b64 s[0:1], fun2
+          v_mov_b32_e32 v0, s1
+          v_add_f32_e32 v0, 1.0, v0
+          fun1_end:
+          s_setpc_b64 s[10:11]
+
+          end:
+          s_endpgm
+        |]
+    cfg
+      `shouldBe` CFG
+        [ BasicBlock -- 0
+            { bbInstructions =
+                [ (0, Instruction ["s", "load", "dwordx2"] [Osgpr [0, 1], Osgpr [4, 5], OConst 0]),
+                  (8, Instruction ["s", "call", "b64"] [Osgpr [10, 11], OConst 10])
+                ],
+              bbPredecessors = [1, 4],
+              bbSuccessors = [7]
+            },
+          BasicBlock -- 1
+            { bbInstructions =
+                [ (12, Instruction ["v", "cmp", "lt", "f32", "e32"] [OCtrl "vcc", OConstF 1.0, Ovgpr [0]]),
+                  (16, Instruction ["s", "cbranch", "vccnz"] [OConst 65531])
+                ],
+              bbPredecessors = [10],
+              bbSuccessors = [0, 2]
+            },
+          BasicBlock -- 2
+            { bbInstructions =
+                [ (20, Instruction ["global", "store", "dword"] [Ovgpr [2, 3], Ovgpr [0], OCtrl "off", OCtrl "offset:4"]),
+                  (28, Instruction ["s", "call", "b64"] [Osgpr [10, 11], OConst 5])
+                ],
+              bbPredecessors = [1],
+              bbSuccessors = [7]
+            },
+          BasicBlock -- 3
+            { bbInstructions = [(32, Instruction ["s", "call", "b64"] [Osgpr [0, 1], OConst 3])],
+              bbPredecessors = [10],
+              bbSuccessors = [6]
+            },
+          BasicBlock -- 4
+            { bbInstructions =
+                [ (36, Instruction ["s", "cmp", "eq", "u32"] [Osgpr [0], OConst 0]),
+                  (40, Instruction ["s", "cbranch", "scc0"] [OConst 65525])
+                ],
+              bbPredecessors = [6],
+              bbSuccessors = [0, 5]
+            },
+          BasicBlock -- 5
+            { bbInstructions = [(44, Instruction ["s", "branch"] [OConst 8])],
+              bbPredecessors = [4],
+              bbSuccessors = [11]
+            },
+          BasicBlock -- 6
+            { bbInstructions = [(48, Instruction ["s", "setpc", "b64"] [Osgpr [0, 1]])],
+              bbPredecessors = [3, 8],
+              bbSuccessors = [4, 9]
+            },
+          BasicBlock -- 7
+            { bbInstructions =
+                [ (52, Instruction ["s", "waitcnt"] [Olgkmcnt 0]),
+                  (56, Instruction ["s", "cmp", "eq", "u32"] [Osgpr [1], OConst 0]),
+                  (60, Instruction ["s", "cbranch", "scc1"] [OConst 3])
+                ],
+              bbPredecessors = [0, 2],
+              bbSuccessors = [8, 10]
+            },
+          BasicBlock -- 8
+            { bbInstructions = [(64, Instruction ["s", "call", "b64"] [Osgpr [0, 1], OConst 65531])],
+              bbPredecessors = [7],
+              bbSuccessors = [6]
+            },
+          BasicBlock -- 9
+            { bbInstructions =
+                [ (68, Instruction ["v", "mov", "b32", "e32"] [Ovgpr [0], Osgpr [1]]),
+                  (72, Instruction ["v", "add", "f32", "e32"] [Ovgpr [0], OConstF 1.0, Ovgpr [0]])
+                ],
+              bbPredecessors = [6],
+              bbSuccessors = [10]
+            },
+          BasicBlock -- 10
+            { bbInstructions = [(76, Instruction ["s", "setpc", "b64"] [Osgpr [10, 11]])],
+              bbPredecessors = [7, 9],
+              bbSuccessors = [1, 3]
+            },
+          BasicBlock -- 11
+            { bbInstructions = [(80, Instruction ["s", "endpgm"] [])],
+              bbPredecessors = [5],
+              bbSuccessors = []
             }
         ]
